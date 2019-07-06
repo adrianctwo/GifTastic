6ae540f50d0e98f987fcf497bde655e85a007755
adrianctwo <adrian.e.situ@gmail.com>
2019-07-05 18:40:50 -0700

    Created function which generate an button based on items in topics array

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     var topics = ["Pikechu", "squirtle"];
 
-    
+    function createButtons() {
+        $("#gifs-buttons").empty();
+        for(i = 0; i < topics.length; i++) {
+            var buttons = $("<button>");
+            buttons.addClass("pokemon-btn");
+            buttons.attr("data", topics[i]);
+            buttons.text(topics[i]);
+
+            $("#gifs-buttons").append(buttons);
+        }
+    }
+
+
+    createButtons();
 })
